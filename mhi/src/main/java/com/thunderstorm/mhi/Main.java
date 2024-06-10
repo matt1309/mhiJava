d43a30eee7c9c006742231322123a25795c4402b
Matt Jayne <mattjayne123@googlemail.com>
2024-06-10 17:17:57 +0100

    .

--- mhi/src/main/java/com/thunderstorm/mhi/Main.java
+++ mhi/src/main/java/com/thunderstorm/mhi/Main.java
@@ -12,8 +12,12 @@ RacParser parser = new RacParser();
 aircon.sethostname("192.168.0.150");
 aircon.setport("5443");
 
+try{
+aircon.parser.translateBytes(aircon, aircon.getAirconStats(false));
+}catch (Exception e){
 
-aircon.parser.translateBytes(aircon.getAirconStats(false));
+    System.out.println(e.toString());
+}
 
 //This would be the part adjusted by user on openhab side. this is turning on the unit. 
 aircon.setOperation(true);
@@ -25,7 +29,7 @@ try{
 //sending command to the aircon unit itself
 String response = aircon.sendAircoCommand(aircon.getAircoId(), command);
 
-aircon.parser.translateBytes(response);
+aircon.parser.translateBytes(aircon, response);
 
 
 
